bd1dba58e560ffe7b3e0ca41d3d011eed881e770
Ellie Liu <ellie301@protonmail.com>
2025-07-12 05:26:43 -0400

    Fixed broken project

---
+++ src/main/java/dev/elliectron/examplemod/ElliesRailImprovementsClient.java
@@ -0,0 +1,31 @@
+package dev.elliectron.examplemod;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = ElliesRailImprovements.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = ElliesRailImprovements.MODID, value = Dist.CLIENT)
+public class ElliesRailImprovementsClient {
+    public ElliesRailImprovementsClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        ElliesRailImprovements.LOGGER.info("HELLO FROM CLIENT SETUP");
+        ElliesRailImprovements.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+}
